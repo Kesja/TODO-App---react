13ff652ef653789ae44db805853d93789bae7959
Joanna Bach <joannakatarzynabach@gmail.com>
2022-03-12 17:04:06 +0100

    Fix container.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,7 +1,24 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
 import styles from './List.module.scss';
 import Columns from '../Columns/Columns';
 
 const List = () => {
+
+  const [columns, setColumns] = useState([
+    { id: 1, title: 'Books', icon: 'book' },
+    { id: 2, title: 'Movies', icon: 'film' },
+    { id: 3, title: 'Games', icon: 'gamepad' }
+  ]);
+
+ /* setTimeout(() => {
+    setColumns([...columns, { id: 4, title: 'Test column'}]);
+  }, 2000);
+
+  useEffect(() => {
+    console.log('Columns have changed');
+}, [columns]);*/
+
     return (
       <div className={styles.list}>
         <header className={styles.header}>
@@ -9,9 +26,7 @@ const List = () => {
           <p className={styles.description}>Interesting things I want to check out</p>
         </header>
         <section className={styles.columns}>
-          <Columns title="Books" icon="book" />
-          <Columns title="Movies" icon="film"/>
-          <Columns title="Games" icon="gamepad"/>
+          {columns.map(column => <Columns key={column.id} title={column.title} icon={column.icon} />)}
         </section>
       </div>
     )
