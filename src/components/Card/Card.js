d2c9eddc722f8fc7f41ec70302a1693c37ee90ea
Joanna Bach <joannakatarzynabach@gmail.com>
2022-04-05 16:13:44 +0200

    Add favorite card functionality

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,8 +1,25 @@
 import styles from './Card.module.scss'
+import clsx from 'clsx';
+import { useDispatch } from 'react-redux';
+import { favoriteClass } from '../../redux/store';
 
 const Card = props => {
+  
+  const dispatch = useDispatch();
+
+  const toggleClass = (cardId) => {
+    dispatch(favoriteClass(cardId))
+  }
+
+
   return (
-    <li className={styles.card}>{props.title}</li>
+    <li className={styles.card}>{props.title}
+      <div>
+        <button type='button' className={clsx(styles.button, props.isFavorite && styles.active)} onClick={() => toggleClass(props.id, props.isFavorite)}>
+        <span className='fa fa-star-o' />
+        </button>
+      </div>
+    </li>
   );
 };
 
--- src/components/Favorite/Favorite.js
+++ src/components/Favorite/Favorite.js
@@ -1,11 +1,34 @@
+import { useSelector } from 'react-redux';
+import { getFavoriteCards } from '../../redux/store';
 import PageTitle from '../PageTitle/PageTitle';
 import styles from './Favorite.module.scss'
+import Card from '../Card/Card';
+import Button from '../Button/Button';
+import { Link } from 'react-router-dom';
 
 const Favorite = () => {
+
+  const favoriteCard = useSelector(getFavoriteCards);
+
+  if (favoriteCard.length === 0) 
+    return (
+      <div>
+        <PageTitle>Favorite</PageTitle>
+        <p className={styles.subtitle}>There is no card!</p>
+        <Link to='/'>
+          <Button>Go to main page</Button>
+        </Link>
+      </div>
+    );
+
 	return (
 		<div className={styles.favorite}>
 			<PageTitle>Favorite</PageTitle>
-			<p className={styles.subtitle}>Lorem ipsum...</p>
+			<div className={styles.column}>
+        <ul className={styles.cards}>
+        {favoriteCard.map(card => <Card key={card.id} title={card.title} id={card.id} isFavorite={card.isFavorite} /> )}
+        </ul>
+      </div>
 		</div>
 	);
 };
